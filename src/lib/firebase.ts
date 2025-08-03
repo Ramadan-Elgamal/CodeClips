dc1b9d54c46e3b3c078e8e2852aabf12357caf40
Ramadan Botsher <ramadan.reda12345@gmail.com>
2025-08-03 06:16:09 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyD09pgld4j2-hOjPKl6rjSY3iKHdTe5mpk",
   authDomain: "codeclips.firebaseapp.com",
   projectId: "codeclips",
-  storageBucket: "codeclips.firebasestorage.app",
+  storageBucket: "codeclips.appspot.com",
   messagingSenderId: "696825099134",
   appId: "1:696825099134:web:2692b1812afb6518d8f92f"
 };
